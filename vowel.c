baad0ec7055384a4ba5cf6f69b265dbd8a2a70bb
Reddyvariashok <40815856+Reddyvariashok@users.noreply.github.com>
2018-07-07 12:33:13 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+char c;
+printf("enter a alphabet");
+scanf("%c",&c);
+is vowel=(c=='a','e','i','o','i','u');
+if(is vowel)
+print("%c" is a vowel,c);
+else
+print("%c is a conconent",c);
+return o:
+}
